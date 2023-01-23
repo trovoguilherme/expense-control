900d6aa597e5990b8c2201ade0ede4cf863d092e
trovo <guinavy2011@live.com>
2023-01-23 20:26:20 -0300

    Adicionando as configurações de trativa das exceptions dos validations constraints

---
+++ src/main/java/br/com/iug/entity/utility/JavaCodes.java
@@ -0,0 +1,13 @@
+package br.com.iug.entity.utility;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class JavaCodes {
+
+    public static final String ERROR_ITEM_MENOR_QUE_ZERO = "item.valor.menor-que-zero";
+    public static final String ERROR_ITEM_NOME_VAZIO = "item.valor.nome-vazio";
+    public static final String ERROR_PARCELA_QTD_PAGA_MENOR_QUE_ZERO = "item.parcela.qtd-paga.menor-que-zero";
+    public static final String ERROR_PARCELA_QTD_RESTANTE_MENOR_QUE_ZERO = "item.parcela.qtd-restante.menor-que-zero";
+
+}
--- src/main/java/br/com/iug/exception/RestExceptionHandler.java
+++ src/main/java/br/com/iug/exception/RestExceptionHandler.java
@@ -3,6 +3,7 @@ package br.com.iug.exception;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -17,6 +18,12 @@ public class RestExceptionHandler {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ResponseException notFoundException(HttpServletRequest request, Exception exception) {
         return new ResponseException(request, resourceNotFound, exception.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseException s(HttpServletRequest request, MethodArgumentNotValidException exception) {
+        return new ResponseException(request, "Problemas com a requisição", exception.getBindingResult().getFieldError().getDefaultMessage());
     }
 
 }
