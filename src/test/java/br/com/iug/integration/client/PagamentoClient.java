c063f32634ca47e0e299cb91a781606ee3e06b37
trovo <guinavy2011@gmail.com>
2024-03-07 00:43:40 -0300

    Criando cliente para testes do Pagamento

---
+++ src/test/java/br/com/iug/integration/client/PagamentoClient.java
@@ -0,0 +1,32 @@
+package br.com.iug.integration.client;
+
+import br.com.iug.entity.request.PagamentoRequest;
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.parsing.Parser;
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.given;
+
+public class PagamentoClient {
+
+    private final RequestSpecBuilder specBuilder;
+
+    public static final String ENDPOINT = "/pagamento";
+
+    public PagamentoClient(int serverPort) {
+        RestAssured.defaultParser = Parser.JSON;
+        specBuilder = new RequestSpecBuilder();
+        specBuilder.addHeader("Content-Type", "application/json");
+        specBuilder.setBaseUri("http://localhost:" + serverPort);
+    }
+
+    public Response create(PagamentoRequest request) {
+        return given()
+                .spec(specBuilder.build())
+                .body(request)
+                .when()
+                .post(ENDPOINT);
+    }
+
+}
